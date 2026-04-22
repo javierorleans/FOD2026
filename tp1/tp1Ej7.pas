22f0c147a1f066d1af010b2250cf43a2fba20198
Javier Ignacio Orleans <javier.orleans@gmail.com>
2026-04-22 20:46:30 -0300

    fin tp1

--- tp1/tp1Ej7.pas
+++ tp1/tp1Ej7.pas
@@ -95,14 +95,14 @@ procedure agregarNovela(var arch: novelas);
 	begin
 	reset(arch);
 	writeln('----- NUEVA NOVELA -----');
-	write('* Ingrese codigo');
+	write('* Ingrese codigo: ');
 	readln(n.codigo);
 	while(existeCodigo(arch, n.codigo))do
 		begin
 		write(' FATAL : Codigo existente. Ingrese otro codigo: ');
 		readln(n.codigo);
 		end;
-	
+	writeln(n.codigo, ' Código VALIDO !!');
 	write('* Ingrese nombre del libro: ');
 	readln(n.nombre);
 	write('* Ingrese genero literario: ');
@@ -110,6 +110,51 @@ procedure agregarNovela(var arch: novelas);
 	write('* Ingrese precio: ');
 	readln(n.precio);
 	seek(arch, filesize(arch));
+	write(arch, n);
+	close(arch);
+	end;
+	
+procedure modificarNovela(var arch: novelas);
+	var
+	n: novela;
+	aux: integer;
+	rta: integer;
+	
+	begin
+	reset(arch);
+	writeln('----- MODIFICACION -----');
+	write('Ingrese código de novela a modificar: ');
+	readln(aux);
+	n.codigo:= -1;
+	if(existeCodigo(arch, aux))then
+		begin		
+		while(n.codigo <> aux)do
+			begin
+			read(arch, n);
+			end;
+		write('modificar 1: nombre | 2: genero | 3: precio ....');
+		readln(rta);
+		case rta of
+			1:  begin
+				write('Escriba nuevo nombre: ');
+				readln(n.nombre);
+				end;
+			2:  begin
+				write('Escriba nuevo genero: ');
+				readln(n.genero);
+				end;
+			3:  begin
+				write('Escriba nuevo precio: ');
+				readln(n.precio);
+				end;
+			else
+				writeln('OPCION INVALIDA .-');
+			end;
+			seek(arch, filepos(arch) - 1);
+			write(arch, n);
+		end
+	else
+		writeln('FATAL : CÓDIGO INGRESADO NO EXISTE !!!');
 	close(arch);
 	end;
  
@@ -121,7 +166,7 @@ opcion: integer;
 
 begin
 assign(archTxt, 'novelas.txt');
-write('Ingrese nombre de archivo: '); //tp1Ej7
+write('Ingrese nombre de archivo: '); // tp1Ej7Novelas
 readln(nomFisico);
 writeln();
 assign(archNovelas, nomFisico);
@@ -133,7 +178,9 @@ while (opcion <> 99) do
 	case opcion of
 		1: importar(archTxt, archNovelas);
 		2: agregarNovela(archNovelas);
+		3: modificarNovela(archNovelas);
 		55: imprimirTodas(archNovelas);
+		99: writeln('- - - GRACIAS, VUELVAS PRONTOS !! - - -');
 		else 
 			writeln('----- OPCION INCORRECTA ..!!!');
 		end;
