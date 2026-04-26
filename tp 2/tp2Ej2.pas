5c198122879690195ef58b46de0ce937424e8dcc
Javier Ignacio Orleans <javier.orleans@gmail.com>
2026-04-26 10:43:20 -0300

    haciendo el tp1ej2

--- tp 2/tp2Ej2.pas
+++ tp 2/tp2Ej2.pas
@@ -3,3 +3,19 @@
 
 {$CODEPAGE UTF8}
 program tp2Ej2;
+
+type
+
+	producto= record
+		codigo: integer;
+		nombre: string[50];
+		precio: real;
+		stockActual: integer;
+		stockMinimo: integer;
+		end;
+		
+	venta= record
+		codigo: integer;
+		cantidadVendida: integer;
+		end;
+		
